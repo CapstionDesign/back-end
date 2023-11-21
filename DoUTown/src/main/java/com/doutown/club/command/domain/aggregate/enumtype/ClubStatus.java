66e4dcb62598a99a5674d1becacb7f36ef343138
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2023-11-22 00:19:34 +0900

    기본 틀 구성 + 마이바티스, 유효성 검사 라이브러리 추가

--- DoUTown/src/main/java/com/doutown/club/command/domain/aggregate/enumtype/ClubStatus.java
+++
@@ -1,6 +0,0 @@
-package com.doutown.club.command.domain.aggregate.enumtype;
-
-public enum ClubStatus {
-
-    승인,반려,대기
-}
--- DoUTown/src/main/java/com/doutown/member/command/application/dto/DeleteMemberDTO.java
+++
@@ -1,4 +0,0 @@
-package com.doutown.member.command.application.dto;
-
-public class DeleteMemberDTO {
-}
