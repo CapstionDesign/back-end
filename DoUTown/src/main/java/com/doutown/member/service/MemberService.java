a6c6cff30591ef9764e669a277c3fa8311f28ddc
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2023-12-08 01:58:10 +0900

    front랑 연동하여 회원가입 되는지 확인 로직

--- DoUTown/src/main/java/com/doutown/member/service/MemberService.java
+++ DoUTown/src/main/java/com/doutown/member/service/MemberService.java
@@ -2,12 +2,14 @@ package com.doutown.member.service;
 
 import com.doutown.member.dao.MemberMapper;
 import com.doutown.member.dto.MemberDTO;
+import com.doutown.student.dto.StudentDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class MemberService {
@@ -18,27 +20,59 @@ public class MemberService {
         this.memberMapper = memberMapper;
     }
 
+    //로그인 확인 로직
+    public boolean isLogin(Map map) {
+        return memberMapper.findByMember(map);
+    }
+
+    //회원가입시 -> 인증 X
     public MemberDTO save(MemberDTO dto) {
         int affected = memberMapper.save(dto);
         return dto;
     }
 
+    //인증 처리
+    public StudentDTO saveStudent(StudentDTO dto){
+        int affected = memberMapper.saveStudent(dto);
+        return dto;
+    }
+
+    //회원 목록 조회
     public List<MemberDTO> findAll() {
         return memberMapper.find();
     }
 
+    //특정 회원 키로 조회
     public MemberDTO getMemberByNo(Long memberNo) {
-        return memberMapper.findByNo(memberNo);
+        return memberMapper.findByMemberNo(memberNo);
     }
 
+    //인증된 학생 목록 조회
+
+    public List<StudentDTO> findStudentAll() {
+        return memberMapper.findStudentAll();
+    }
+
+    //인증된 특정 학생 조회
+    public StudentDTO getStudentByNo(Long studentNo) {
+        return memberMapper.findByStudentNo(studentNo);
+    }
+
+
+    //회원 탈퇴
     public int removeMember(Long memberNo) {
         int affected = 0;
-        MemberDTO memberDTO = memberMapper.findByNo(memberNo);
+        MemberDTO memberDTO = memberMapper.findByMemberNo(memberNo);
         affected = memberMapper.deleteMember(memberDTO.getMemberName());
         return affected;
     }
 
+
+    //회원 정보 수정
     public int updateMember(Long memberNo) {
+
+
+
         int affected = 0;
         memberMapper.updateMember(memberNo);
         return affected;
