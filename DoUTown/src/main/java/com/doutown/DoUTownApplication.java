4fffc77bbb1987ffcf3ef17c02024db491093b57
chodonghoon2 <112090479+chodonghoon2@users.noreply.github.com>
2023-12-12 22:10:52 +0900

    cors

--- DoUTown/src/main/java/com/doutown/DoUTownApplication.java
+++ DoUTown/src/main/java/com/doutown/DoUTownApplication.java
@@ -2,6 +2,9 @@ package com.doutown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DoUTownApplication {
@@ -9,5 +12,21 @@ public class DoUTownApplication {
     public static void main(String[] args) {
         SpringApplication.run(DoUTownApplication.class, args);
     }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+
 
 }
